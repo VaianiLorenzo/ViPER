3b5841021f5c5daf34c8db85d54575892afbf74c
VaianiLorenzo <lorenzo_vaiani@yahoo.com>
2022-06-09 11:12:16 +0000

    frame crop script fixed

--- crop_images.py
+++ crop_images.py
@@ -1,9 +1,12 @@
 import argparse
 import torch
 import glob
+import sys
+sys.path.append("yoloface")
 from yoloface.face_detector import YoloDetector
 import numpy as np
 from PIL import Image
+import os
 
 
 parser = argparse.ArgumentParser(description="Crop images detectiong faces with YOLOv5")
@@ -25,15 +28,20 @@ args = parser.parse_args()
 if not os.path.exists(args.output_folder):
     os.mkdir(args.output_folder)
 
-
-for f_name in glob.glob(f"{args.input_folder}/*"):
-    try:
-        model = YoloDetector(target_size=720,gpu=device,min_face=90)
-        orgimg = np.array(Image.open(f_name))
-        bboxes,points = model.predict(orgimg)
-        img = Image.open(f_name)
-        target_f_name = f_name.replace(args.input_folder, args.output_folder)
-        img.crop(bboxes[0][0]).save(target_f_name)
-    except Exception as e:
-        print (e)
-
+for video in os.listdir(args.input_folder):
+    if not os.path.exists(os.path.join(args.output_folder, video)):
+        os.mkdir(os.path.join(args.output_folder, video))
+    for frame in os.listdir(os.path.join(args.input_folder, video)):
+        try:
+            model = YoloDetector(target_size=720,gpu=device,min_face=90)
+            path = os.path.join(args.input_folder, video, frame)
+            orgimg = np.array(Image.open(path))
+            bboxes,points = model.predict(orgimg)
+            img = Image.open(path)
+            target_f_name = path.replace(args.input_folder, args.output_folder)
+            if len(bboxes[0]) == 0:
+                img.save(target_f_name)
+            else:
+                img.crop(bboxes[0][0]).save(target_f_name)
+        except Exception as e:
+            print (e)
